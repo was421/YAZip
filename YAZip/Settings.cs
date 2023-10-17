6ac2219a590c24d3468f16c5523ed78d677e79c5
was421 <was421.1@gmail.com>
2023-10-16 21:55:24 -0500

    missing file

---
+++ YAZip/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAZip
+{
+    public sealed class Settings
+    {
+        private Settings() { }
+        private static Settings instance = null;
+        public static Settings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Settings();
+                }
+                return instance;
+            }
+        }
+
+        public bool ds3comply = false;
+
+        public string key =@"";
+    }
+}
